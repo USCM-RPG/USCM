26e63420cce5eb51c578c148e71bb06f6cb3c985
uscmbrixton <oskar.grindemyr@gmail.com>
2025-01-04 22:48:28 +0100

    getTerrain

--- controllers/mission_controller.php
+++ controllers/mission_controller.php
@@ -292,6 +292,28 @@ class MissionController {
     } catch (PDOException $e) {
     }
   }
+  
+  public function getTerrain($mission) {
+	$expertisearray = array();
+	$expertisesql = "SELECT en.id,expertise_name, expertise_group_id, value FROM expertise_names en
+              JOIN terrain_mission tm ON tm.expertise_id=en.id 
+              JOIN expertise_groups eg ON en.expertise_group_id=eg.id
+              WHERE tm.mission_id=:missionId AND eg.expertise_group_name='Terrain';";
+	$db = getDatabaseConnection();
+	$stmt = $db->prepare($expertisesql);
+    $stmt->bindValue(':missionId', $mission->getId(), PDO::PARAM_INT);
+    $stmt->execute();
+    while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+	  $expertise = new Expertise();
+	  $expertise->setId($row['id']);
+	  $expertise->setExpertiseGroupId($row['expertise_group_id']);
+	  $expertise->setName($row['expertise_name']);
+	  $expertise->setValue($row['value']);
+	  $expertisearray[] = $expertise;
+    }
+    
+    return $expertisearray;
+  }
 
   /**
    *
