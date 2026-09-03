2f606b1bbd9daeaa3709622be3efb55002ef35b6
Kristoffer Nordström <totte.nordstrom@gmail.com>
2026-09-03 11:44:46 +0200

    feat: add create player link on create character page

--- pages/characters/create_v2.php
+++ pages/characters/create_v2.php
@@ -8,7 +8,14 @@ $userController = new UserController();
 $user = $userController->getCurrentUser();
 
 if ($user->isAdmin() || $user->isGm()) { ?>
-  <h1 class="heading heading-h1">Create character</h1>
+  <h1 class="heading heading-h1">
+    Create character
+    <?php if ($_SESSION['level']>=3) { ?>
+      <span class="span">
+        <a href="index.php?url=player/create.php">Create player</a>
+      </span>
+    <?php } ?>
+  </h1>
 
 <form class="form" method="post" action="actions/character.php?action=create_character">
   <div class="grid grid--1x2">
