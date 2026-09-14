c7fbd622c23e0f6dd00535aea6f2ef571aedd2da
Kristoffer Nordström <totte.nordstrom@gmail.com>
2026-09-15 01:28:21 +0200

    refactor: functions, move single use to source

--- functions.php
+++ functions.php
@@ -31,7 +31,6 @@ require_once ("controllers/rank.php");
 require_once ("controllers/tag.php");
 
 $db_connection = NULL;
-// set_exception_handler(die("Caught exception, going to die"));
 
 /**
  * Returns a new or existing database connection
@@ -48,110 +47,6 @@ function getDatabaseConnection() {
         $GLOBALS['db_user'], $GLOBALS['db_password']);
     return $db_connection;
   }
-}
-
-function validate($level) {
-  if (is_numeric($_SESSION['level'])) {
-    if ($_SESSION['level'] >= $level) {
-      return 1;
-    } else {
-      return 0;
-    }
-  } else {
-    if ($_SESSION['level'] == "konventsadmin") {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-}
-/*
- * Levels:
- * 0: Logged out
- * 1: Registered user
- * 2: Game master
- * 3: Site admin
- */
-function login($level) {
-  $db = getDatabaseConnection();
-  if (isset($_GET['alt'])) {
-    $alt = $_GET['alt'];
-    if ($alt == "logout") {
-      $_SESSION['inloggad'] = 0;
-      $_SESSION['level'] = "0";
-      unset($_SESSION['anvandarnamn']);
-      unset($_SESSION['table_prefix']);
-      unset($_SESSION['user_id']);
-      unset($_SESSION['platoon_id']);
-      session_unset();
-      session_destroy();
-      return 0;
-    } elseif ($alt == "login") {
-      if (empty($_POST['anvandarnamn']) || empty($_POST['losenord'])) {
-        $_SESSION['inloggad'] = 0;
-        $_SESSION['level'] = "0";
-        session_unset();
-        session_destroy();
-        return 0;
-      }
-
-        $query = "SELECT Users.id, emailadress, platoon_id, Admins.userid AS Admin, GMs.userid AS GM,
-                logintime, count(Users.id) AS howmany
-                FROM Users
-                LEFT JOIN Admins ON Admins.userid=Users.id
-                LEFT JOIN GMs ON GMs.userid=Users.id AND GMs.active=1
-                WHERE emailadress=:userName
-                AND `password`=password(:password)
-                GROUP BY Users.id";
-        $stmt = $db->prepare($query);
-        $stmt->bindValue(':userName', $_POST['anvandarnamn'], PDO::PARAM_STR);
-        $stmt->bindValue(':password', $_POST['losenord'], PDO::PARAM_STR);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if (!is_null($row['howmany'] ) && $row['howmany'] == 1) {
-          $userinfo = $row;
-          if ($userinfo['Admin']) {
-            $userlevel = 3;
-          } elseif ($userinfo['GM']) {
-            $userlevel = 2;
-          } else {
-            $userlevel = 1;
-          }
-          $_SESSION['anvandarnamn'] = $_POST['anvandarnamn'];
-          $_SESSION['inloggad'] = 1;
-          $_SESSION['level'] = $userlevel;
-          $_SESSION['table_prefix'] = $_POST['rpg'];
-          $_SESSION['user_id'] = $userinfo['id'];
-          $_SESSION['platoon_id'] = $userinfo['platoon_id'];
-          $db->exec("UPDATE Users SET lastlogintime = '{$userinfo['logintime']}' WHERE id = {$userinfo['id']}");
-          $db->exec("UPDATE Users SET logintime = NOW() WHERE id = {$userinfo['id']}");
-          return 1;
-        } elseif (!is_null($row['howmany'] ) && $row['howmany'] > 1) {
-          $_SESSION['inloggad'] = 0;
-          $_SESSION['level'] = "0";
-          return 0;
-        } else {
-          $_SESSION['inloggad'] = 0;
-          $_SESSION['level'] = "0";
-          return 0;
-        }
-    }
-  }
-}
-
-/*
- * Returns all file names under the given directory, recursively, stripped of the base dir.
- * Used for checking validity of includes from GET variables
- */
-function recursive_dirlist($base_dir) {
-  $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($base_dir, FilesystemIterator::SKIP_DOTS));
-  $filelist = array();
-  foreach ( $files as $file ) {
-    if ($file->isFile()) {
-      $filelist[] = str_replace($base_dir, '', $file->getPathname());
-    }
-  }
-  return $filelist;
 }
 
 function attribute2visible($attributearray) {
